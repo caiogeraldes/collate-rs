ac538341ffb5295fbb6521eead805e413d34f601
Caio Geraldes <caio.geraldes@usp.br>
2022-12-18 21:14:38 -0300

    documentation: definitions-todo-list

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,55 @@ pub mod conformance;
 ///
 /// The Unicode Collation Algorithm depends on the concept of mapping characters in Unicode strings to sequences of collation weights called sort keys. Those sort keys, in turn, can be directly compared to determine the relative order of the strings. This section provides precise definitions of the special terminology used in the algorithm and its intermediate steps, along with explanation of the notation used in examples and in the discussion of the algorithm.
 ///
+/// ## List of implemented definitions:
+///
+///  - [x] UTS10-D1
+///  - [x] UTS10-D2
+///  - [x] UTS10-D3
+///  - [x] UTS10-D4
+///  - [x] UTS10-D5
+///  - [x] UTS10-D6
+///  - [x] UTS10-D7
+///  - [x] UTS10-D8
+///  - [x] UTS10-D9
+///  - [x] UTS10-D10
+///  - [x] UTS10-D11
+///  - [x] UTS10-D12
+///  - [x] UTS10-D13
+///  - [x] UTS10-D14
+///  - [x] UTS10-D15
+///  - [ ] UTS10-D16
+///  - [x] UTS10-D17
+///  - [x] UTS10-D18
+///  - [x] UTS10-D19
+///  - [x] UTS10-D20
+///  - [x] UTS10-D21
+///  - [x] UTS10-D22
+///  - [ ] UTS10-D23
+///  - [ ] UTS10-D24
+///  - [ ] UTS10-D25
+///  - [ ] UTS10-D26
+///  - [ ] UTS10-D27
+///  - [ ] UTS10-D28
+///  - [ ] UTS10-D29
+///  - [ ] UTS10-D30
+///  - [ ] UTS10-D31
+///  - [ ] UTS10-D32
+///  - [ ] UTS10-D33
+///  - [ ] UTS10-D34
+///  - [ ] UTS10-D35
+///  - [ ] UTS10-D36
+///  - [ ] UTS10-D37
+///  - [ ] UTS10-D38
+///  - [ ] UTS10-D39
+///  - [ ] UTS10-D40
+///  - [ ] UTS10-D41
+///  - [ ] UTS10-D42
+///  - [ ] UTS10-D43
+///  - [ ] UTS10-D44
+///  - [ ] UTS10-D45
+///  - [ ] UTS10-D46
+///
 pub mod definitions;
 // 4
 #[cfg(feature = "todo")]
