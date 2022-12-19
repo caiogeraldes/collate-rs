cafff570e198bd8282b3a3b476b45e703ec2019d
Caio Geraldes <caio.geraldes@usp.br>
2022-12-18 21:08:15 -0300

    implements: UTS10-D16 to D22

--- src/definitions/mod.rs
+++ src/definitions/mod.rs
@@ -29,7 +29,7 @@ impl Ignorable for CollationWeight {
 }
 
 /// UTS10-D2. Collation Element: An ordered list of collation weights.
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct CollationElement(Vec<CollationWeight>);
 
 impl CollationElement {
@@ -203,6 +203,135 @@ impl Ignorable for CollationLevel {
 /// Ignorable weights are passed over by the rules that construct sort keys from sequences of collation elements. Thus, their presence in collation elements does not impact the comparison of strings using the resulting sort keys. The judicious assignment of ignorable weights in collation elements is an important concept for the UCA.
 pub trait Ignorable {
     fn is_ignorable(&self) -> bool;
+}
+
+/// UTS10-D16. Variable Collation Element: A primary collation element with a low (but non-zero) value for its primary weight.
+///
+/// > Low primary weights are generally reserved for punctuation and symbols, to enable special handling of those kinds of characters. Variable collation elements are subject to special rules when constructing sort keys. See Section 4, Variable Weighting. In the Default Unicode Collation Element Table [Allkeys](https://www.unicode.org/reports/tr10/#Allkeys) the primary weights of all variable collation elements are prefixed with an asterisk instead of a dot, so that they can be clearly identified.
+///
+/// The relationship between these terms for patterns of ignorable weights in collation elements, together with schematic examples of the corresponding collation elements, is shown in the following table, constructed on the assumption that collation elements have four collation levels. Note that quaternary collation elements have the same schematic pattern of weights as variable collation elements which have been shifted.
+///
+/// | Schematic Example      | Main Term                                | General Type  | Level Notation    |
+/// |------------------------|------------------------------------------|---------------|-------------------|
+/// | [.nnnn.nnnn.nnnn.nnnn] | Primary Collation Element                | Non-ignorable | Level 0 Ignorable |
+/// | [*nnnn.nnnn.nnnn.nnnn] | Variable Collation Element (not shifted) | Non-ignorable | Level 0 Ignorable |
+/// | [.0000.nnnn.nnnn.nnnn] | Secondary Collation Element              | Ignorable     | Level 1 Ignorable |
+/// | [.0000.0000.nnnn.nnnn] | Tertiary Collation Element               | Ignorable     | Level 2 Ignorable |
+/// | [.0000.0000.0000.nnnn] | Quaternary Collation Element             | Ignorable     | Level 3 Ignorable |
+/// | [.0000.0000.0000.nnnn] | Variable Collation Element (shifted)     | Ignorable     | Level 3 Ignorable |
+/// | [.0000.0000.0000.0000] | Completely Ignorable Collation Element   | Ignorable     | Level 4 Ignorable |
+///
+// TODO: Assert is adequate
+
+pub struct VariableCollationElement {
+    collation_element: CollationElement,
+}
+
+impl VariableCollationElement {
+    pub fn collation_element(&self) -> &CollationElement {
+        &self.collation_element
+    }
+}
+
+/// UTS10-D17. Collation Element Mapping: A mapping from one (or more) Unicode characters to one (or more) collation elements.
+///
+/// Effectively, a given collation element table defines a mathematical function. It is instantiated as a list of collation element mappings. Collectively, the input for those mappings, which generally consists of all Unicode code points plus some well-defined set of short sequences of Unicode characters, constitutes the domain of the function. Collectively, the output for those mappings consists of a defined list of collation element sequences; the set of which constitutes the codomain of the function. And the collation element table itself constitutes the graph of the function.
+///
+/// > Note: For formal completeness, a collation element mapping is usually defined to include in its domain all Unicode code points, including noncharacters and unassigned, reserved code points. The Unicode Collation Algorithm specifies the mapping for unassigned code points, as well as some ranges of assigned characters, by means of implicit weights. See Section 10, Weight Derivation for details. However, because specific collation element tables such as the Default Unicode Collation Element Table (DUCET) generally only contain a list of collation element mappings for assigned characters, and maps those assigned characters to collation elements with explicit weights, the definitions in this section are simplified by referring to the input values just in terms of "Unicode characters".
+///
+/// Collation element mappings are divided into subtypes, based on a distinction between whether the input of the mapping constitutes a single Unicode character or a sequence of Unicode characters, and a separate distinction between whether the output of the mapping constitutes a single collation element or a sequence of collation elements.
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum CollationElementMapping {
+    /// UTS10-D18. Simple Mapping: A collation element mapping from one Unicode character to one collation element.  collation_element: Vec<CollationElement>,
+    SimpleMapping {
+        character: char,
+        collation_element: CollationElement,
+    },
+    /// UTS10-D19. Expansion: A collation element mapping from one Unicode character to a sequence of more than one collation element.
+    Expansion {
+        character: char,
+        collation_elements: Vec<CollationElement>,
+    },
+    /// UTS10-D20. Many-to-One Mapping: A collation element mapping from more than one Unicode character to one collation element.
+    ManyToOneMapping {
+        characters: Vec<char>,
+        collation_element: CollationElement,
+    },
+    /// UTS10-D21. Many-to-Many Mapping: A collation element mapping from more than one Unicode character to a sequence of more than one collation element.
+    ManyToManyMapping {
+        characters: Vec<char>,
+        collation_elements: Vec<CollationElement>,
+    },
+}
+
+impl CollationElementMapping {
+    pub fn new(characters: &[char], collation_elements: &[CollationElement]) -> Result<Self> {
+        let mut cem = Self::ManyToManyMapping {
+            characters: characters.to_vec(),
+            collation_elements: collation_elements.to_vec(),
+        };
+        cem.simplify()?;
+        Ok(cem)
+    }
+
+    /// UTS10-D22. Contraction: Either a many-to-one mapping or a many-to-many mapping.
+    pub fn is_contraction(&self) -> bool {
+        match self {
+            Self::ManyToOneMapping { .. } | Self::ManyToManyMapping { .. } => true,
+            _ => false,
+        }
+    }
+
+    /// Helper function to convert misassigned [`CollationElementMapping`]s
+    fn simplify(&mut self) -> Result<()> {
+        match self {
+            Self::SimpleMapping { .. } => {}
+            Self::Expansion {
+                character,
+                collation_elements,
+            } => {
+                if collation_elements.len() == 1 {
+                    *self = Self::SimpleMapping {
+                        character: *character,
+                        collation_element: collation_elements[0].clone(),
+                    };
+                }
+            }
+            Self::ManyToOneMapping {
+                characters,
+                collation_element,
+            } => {
+                if characters.len() == 1 {
+                    *self = Self::SimpleMapping {
+                        character: characters[0],
+                        collation_element: collation_element.clone(),
+                    }
+                }
+            }
+            Self::ManyToManyMapping {
+                characters,
+                collation_elements,
+            } => {
+                if characters.len() == 1 && collation_elements.len() == 1 {
+                    *self = Self::SimpleMapping {
+                        character: characters[0],
+                        collation_element: collation_elements[0].clone(),
+                    };
+                } else if characters.len() == 1 && collation_elements.len() > 1 {
+                    *self = Self::Expansion {
+                        character: characters[0],
+                        collation_elements: collation_elements.to_vec(),
+                    }
+                } else if characters.len() > 1 && collation_elements.len() == 1 {
+                    *self = Self::ManyToOneMapping {
+                        characters: characters.to_vec(),
+                        collation_element: collation_elements[0].clone(),
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
 }
 
 #[cfg(test)]
